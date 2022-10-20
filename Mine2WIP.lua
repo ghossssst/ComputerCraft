d4937f643057ef22de42504ae4bed2e1f2569bbe
Luke Wheeler <115876478+ghossssst@users.noreply.github.com>
2022-10-20 14:13:19 +0100

    Mine2WIP

--- Mine2WIP.lua
+++ Mine2WIP.lua
@@ -1,11 +1,32 @@
+
+--takes input for mine length
 io.write("Enter mine length: ")
 length = io.read()
 
+--hashtable of all ores the turtle will look to mine. 
 targetores = {}
-targetores["minecraft:Diamond_ore"] = true 
-targetores["minecraft:Iron_ore"] = true
-targetores["minecraft:"]
+targetores["minecraft:diamond_ore"] = true
+targetores["minecraft:iron_ore"] = true
+targetores["minecraft:copper_ore"] = true
+targetores["minecraft:gold_ore"] = true
+targetores["minecraft:coal_ore"] = true
+targetores["minecraft:lapis_ore"] = true
+targetores["minecraft:redstone_ore"] = true
+targetores["minecraft:lit_redstone_ore"] = true
+targetores["minecraft:emerald_ore"] = true
+targetores["minecraft:quartz_ore"] = true
+targetores["minecraft:nether_gold_ore"] = true
+targetores["minecraft:ancient_debris"] = true
+targetores["minecraft:deepslate_coal_ore"] = true
+targetores["minecraft:deepslate_copper_ore"] = true
+targetores["minecraft:deepslate_diamond_ore"] = true
+targetores["minecraft:deepslate_emerald_ore"] = true
+targetores["minecraft:deepslate_gold_ore"] = true
+targetores["minecraft:deepslate_iron_ore"] = true
+targetores["minecraft:deepslate_lapis_ore"] = true
+targetores["minecraft:deepslate_redstone_ore"] = true
 
+--refuels from the turtles first inventory slot then prints the current fuel level
 function fuelInfo()
     fuel = turtle.getFuelLevel()
     while fuel < 200 do
@@ -13,56 +34,51 @@ function fuelInfo()
     end
     print(fuel)
 end
-    
 
+--detects if the turtle is facing a block.
+--if the turtle is facing a block it will mine that block and move forward into that space.
+--once the turtle has moved forward it will inspect the blocks around it to see if they are listed in the targetores hashtable.
+--if listed in the hashtable the turtle will mine those blocks.
 function dig()
     for i = 1,length do
         if turtle.detect() then
             turtle.dig()
             turtle.forward()
-            do success, data = turtle.inspectUp()
-                if targetores[data.name] do
+            do local success, data = turtle.inspectUp()
+                if targetores[data.name] then
                     turtle.digUp()
                 end
-            do meta, name = turtle.inspectDown()
-                if data.name == minecraft:Diamondore do
-                    turtle.digDown()
+            end
+            do local success, data = turtle.inspectDown()
+                if targetores[data.name] then
+                    turtle.digUp()
                 end
-            do turtle.turnLeft
-                do meta, name = turtle.inspect()
-                    if name == minecraft:Diamondore do
-                        turtle.dig()
+            end
+            do turtle.turnLeft()
+                do local success, data = turtle.inspect()
+                    if targetores[data.name] then
+                        turtle.digUp()
                     end
                 end
-            do turtle.turnRight
-            do turtle.turnRight
-                do meta, name = turtle.inspect()
-                    if name == minecraft:Diamondore do
-                        turtle.dig()
+            end
+            do turtle.turnRight()
+            end
+            do turtle.turnRight()
+                do local success, data = turtle.inspect()
+                    if targetores[data.name] then
+                        turtle.digUp()
                     end
                 end
-            do turtle.turnLeft
+            end
+            do turtle.turnLeft()
+            end
         end
     end
-
 end
     
-function Set (list)
-    local set = {}
-    for _, l in ipairs(list) do set[l] = true end
-    return set
-  end
-
+--calls the fuelinfo function.
 fuelInfo()
 
+--calls the dig function.
 dig()
 
-
-targetores = {"Diamond_ore", }
-
---success, blockinfo = turtle.inspect()
---if success then
---    if data.blockinfo.name == Diamond
---end
---turtle.dig()
---turtle.forward()
