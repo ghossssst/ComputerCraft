8416b9f4ff80989539dc9d8f2eae0fdc361ba534
Luke Wheeler <115876478+ghossssst@users.noreply.github.com>
2022-10-20 16:42:13 +0100

    Mine2WIP

--- Mine2WIP.lua
+++ Mine2WIP.lua
@@ -1,7 +1,9 @@
 
 --takes input for mine length
 io.write("Enter mine length: ")
-length = io.read()
+lengthinput = io.read()
+
+length = lengthinput / 2
 
 --hashtable of all ores the turtle will look to mine. 
 targetores = {}
@@ -30,6 +32,7 @@ targetores["minecraft:deepslate_redstone_ore"] = true
 function fuelInfo()
     fuel = turtle.getFuelLevel()
     while fuel < 200 do
+        turtle.select(1)
         turtle.refuel(1)
     end
     print(fuel)
@@ -40,37 +43,139 @@ end
 --once the turtle has moved forward it will inspect the blocks around it to see if they are listed in the targetores hashtable.
 --if listed in the hashtable the turtle will mine those blocks.
 function dig()
+    for y = 1,length, 12 do
+        turtle.select(2)
+        turtle.turnLeft(2)
+        turtle.place(2)
+    end
     for i = 1,length do
         if turtle.detect() then
             turtle.dig()
             turtle.forward()
-            do local success, data = turtle.inspectUp()
-                if targetores[data.name] then
-                    turtle.digUp()
-                end
-            end
+--            do local success, data = turtle.inspectUp()
+--                if targetores[data.name] then
+--                    turtle.digUp()
+--                end
+--            end
             do local success, data = turtle.inspectDown()
                 if targetores[data.name] then
-                    turtle.digUp()
+                    turtle.digDown()
                 end
             end
             do turtle.turnLeft()
                 do local success, data = turtle.inspect()
                     if targetores[data.name] then
-                        turtle.digUp()
+                        turtle.dig()
                     end
                 end
             end
-            do turtle.turnRight()
-            end
-            do turtle.turnRight()
+            do turtle.turnRight(2)
                 do local success, data = turtle.inspect()
                     if targetores[data.name] then
-                        turtle.digUp()
+                        turtle.dig()
                     end
                 end
             end
             do turtle.turnLeft()
+            end
+        end
+
+------------------------------------------------------
+
+            if turtle.detectUp() then
+                turtle.digUp()
+                turtle.up()
+                do local success, data = turtle.inspectUp()
+                    if targetores[data.name] then
+                        turtle.digUp()
+                    end
+                end
+--                do local success, data = turtle.inspectDown()
+--                    if targetores[data.name] then
+--                        turtle.digDown()
+--                    end
+--                end
+                do turtle.turnLeft()
+                    do local success, data = turtle.inspect()
+                        if targetores[data.name] then
+                            turtle.dig()
+                        end
+                    end
+                end
+                do turtle.turnRight(2)
+                    do local success, data = turtle.inspect()
+                        if targetores[data.name] then
+                            turtle.dig()
+                        end
+                    end
+                end
+                do turtle.turnLeft()
+                end
+            end
+
+------------------------------------------------------
+
+            if turtle.detect() then
+                turtle.dig()
+                turtle.move()
+                do local success, data = turtle.inspectUp()
+                    if targetores[data.name] then
+                        turtle.digUp()
+                    end
+                end
+--                do local success, data = turtle.inspectDown()
+--                    if targetores[data.name] then
+--                        turtle.digDown()
+--                    end
+--                end
+                do turtle.turnLeft()
+                    do local success, data = turtle.inspect()
+                        if targetores[data.name] then
+                            turtle.dig()
+                        end
+                    end
+                end
+                do turtle.turnRight(2)
+                    do local success, data = turtle.inspect()
+                        if targetores[data.name] then
+                            turtle.dig()
+                        end
+                    end
+                end
+                do turtle.turnLeft()
+                end
+
+                ------------------------------------------------------
+
+            if turtle.detectDown() then
+                turtle.digDown()
+                turtle.down()
+--                do local success, data = turtle.inspectUp()
+--                    if targetores[data.name] then
+--                        turtle.digUp()
+--                    end
+--                end
+                do local success, data = turtle.inspectDown()
+                    if targetores[data.name] then
+                        turtle.digDown()
+                    end
+                end
+                do turtle.turnLeft()
+                    do local success, data = turtle.inspect()
+                        if targetores[data.name] then
+                            turtle.dig()
+                        end
+                    end
+                end
+                do turtle.turnRight(2)
+                    do local success, data = turtle.inspect()
+                        if targetores[data.name] then
+                            turtle.dig()
+                        end
+                    end
+                end
+                do turtle.turnLeft()
+                end
             end
         end
     end
